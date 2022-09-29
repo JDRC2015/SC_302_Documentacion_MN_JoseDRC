51566b9bab2905924078fa7bfb87c25af53d8d82
JDRC2015 <114629644+JDRC2015@users.noreply.github.com>
2022-09-28 19:56:40 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -9,3 +9,26 @@ alert("hola mundo_editado");
 Console.log(“cambios en el archivo”);
 
 
+// Calendario jquery
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/calendar.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/calendar-starting-monday.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/calendar.css">
+      $('#calendar').calendar();
+    <div id="calendar"></div>
+    $('#calendar').calendar({date: '2017-01-01'});
+    $('#calendar').calendar({color: 'orange'});
+
+    /* the default color scheme is Turquoise */
+    $('#calendar').calendar({color: 'yellow'});
+    $('#calendar').calendar({color: 'pink'});
+    $('#calendar').calendar({color: 'purple'});
+    $('#calendar').calendar({color: 'blue'});
+    $('#calendar').calendar({color: 'green'});
+    $('#calendar').calendar({color: 'grey'});
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/calendar.min.js"></script>
+
+
